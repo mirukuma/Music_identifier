c39d94994f3d7b6e857c836b9b01dcf50f02f28d
mirukuma <mirukuma_tech@icloud.com>
2021-09-23 00:00:33 +0900

    /music/{id}でそれぞれの曲データを取得できるように

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 	"log"
 	"encoding/json"
+	"github.com/gorilla/mux"
 )
 
 type Music struct {
-	  Id int `json:"id"` // 曲のid
+	  Id string `json:"id"` // 曲のid
     Title string `json:"Title"` // 曲のタイトル
     Desc string `json:"desc"` // 曲の説明や，YouTubeなりApple MusicなりのURLなど
     Content []int `json:"content"` // メロディーライン（形式は要検討）
@@ -20,19 +21,32 @@ func hello(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "hello, world!\n")
 }
 
-func returnAllMusics(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnAllArticles")
+func returnAllMusics(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(Musics)
+}
+
+func returnSingleMusic(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+    key := vars["id"]
+
+    for _, music := range Musics {
+    	if music.Id == key {
+    		fmt.Fprint(w, music)
+    	}
+    }
 }
 
 
 func main() {
 	Musics = []Music{
-      Music{Id: 0, Title: "いつもの曲", Desc: "https://music.apple.com/jp/album/tema-obu-isutansutori/1437763647?i=1437763752&l=en", Content: []int{57, 62, 64, 67, 64, 62, 59, 60, 59, 55}},
-      Music{Id: 1, Title: "夏影", Desc: "https://music.apple.com/jp/album/natukage/818439884?i=818439888&l=en", Content: []int{60, 55, 60, 62, 64, 67, 62, 62, 64, 60, 62, 64}},
+      Music{Id: "0", Title: "いつもの曲", Desc: "https://music.apple.com/jp/album/tema-obu-isutansutori/1437763647?i=1437763752&l=en", Content: []int{57, 62, 64, 67, 64, 62, 59, 60, 59, 55}},
+      Music{Id: "1", Title: "夏影", Desc: "https://music.apple.com/jp/album/natukage/818439884?i=818439888&l=en", Content: []int{60, 55, 60, 62, 64, 67, 62, 62, 64, 60, 62, 64}},
   }
 
-  http.HandleFunc("/", hello)
-  http.HandleFunc("/all", returnAllMusics)
-  log.Fatal(http.ListenAndServe(":10000", nil))
+  myRouter := mux.NewRouter()
+
+  myRouter.HandleFunc("/", hello)
+  myRouter.HandleFunc("/all", returnAllMusics)
+  myRouter.HandleFunc("/music/{id}", returnSingleMusic)
+  log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
